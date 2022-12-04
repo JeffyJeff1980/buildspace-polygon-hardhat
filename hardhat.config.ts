f6cd586e366d033bfc96f9497f8e5380c18a89e2
Jean-Francois Beaulieu <jfpb1980@gmail.com>
2022-12-04 09:50:41 -0500

    minor changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,29 @@ require("dotenv").config({ path: ".env" });
 module.exports = {
   solidity: "0.8.10",
   networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_RINKEBY_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    ropsten: {
+      url: process.env.INFURA_ROPSTEN_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.ALCHEMY_MAINNET_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
     mumbai: {
       url: process.env.ALCHEMY_POLYGON_MUMBAI_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    matic: {
+      url: process.env.ALCHEMY_POLYGON_MAINNET_API_KEY_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
